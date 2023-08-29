ef4190beba397899d9c5a9b9cf7ea31c85c276f7
Liyabona Mxhalisa <mxhalisalm@gmail.com>
2023-08-29 14:48:16 +0200

    bycripts and stuff

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,34 @@
 const { User } = require("../models")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const userController = {
-  // Register a new user
   async register(req, res) {
     try {
-      const user = await User.create(req.body)
-      return res.status(201).json(user)
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(req.body.password, 10) // 10 is the number of salt rounds
+
+      // Create a user with the hashed password
+      const user = await User.create({
+        ...req.body,
+        password: hashedPassword,
+      })
+
+      // Generate JWT token using secret from environment variables
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      })
+
+      // Respond with user data (without password) and the token
+      return res.status(201).json({
+        user: {
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+        },
+        token,
+      })
     } catch (error) {
       return res.status(500).json({ message: "Error registering user." })
     }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const userController = require("../controllers/userController")
+const { check } = require("express-validator")
 const router = express.Router()
 
-router.post("/register", userController.register)
+router.post(
+  "/register",
+  [
+    check("email").isEmail().withMessage("Enter a valid email"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.register
+)
+
 router.get("/", userController.getAllUsers) // This is just for testing and should be removed in production!
 
 module.exports = router
