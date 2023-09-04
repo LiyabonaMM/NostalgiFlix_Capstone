1dfb2b0c0944237bf763822136004ad44021c661
Liyabona Mxhalisa <mxhalisalm@gmail.com>
2023-09-04 11:29:10 +0200

    First commit in the Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+Footer {
+  margin-top: auto;
+}
+</style>
